47e1f5dce999be2ab5892faf1aa4c55d3b6a357d
cfernandeznoguchi <camilaferno1999@gmail.com>
2022-04-16 11:30:54 -0400

    integrating RFID to weight sensor

--- SCCB_Database/Scripts.sql
+++ SCCB_Database/Scripts.sql
@@ -4,11 +4,11 @@
 BEGIN;
 
 CREATE TABLE USER (
-	UID int(12) NOT NULL PRIMARY KEY,
+	UID TEXT NOT NULL PRIMARY KEY,
 	firstName TEXT NOT NULL,
 	lastName TEXT NOT NULL,
 	username TEXT,
-	userPassword TEXT,
+	userPassword TEXT
 );
 
 CREATE TABLE POINT (
@@ -19,6 +19,7 @@ CREATE TABLE POINT (
 	FOREIGN KEY (UID) REFERENCES USER(UID)
 );
 
-INSERT INTO USER VALUES (188435878401,"John", "Doe", "jdoe@bentley.edu", MD5("Jdoe22!"));
-INSERT INTO USER VALUES (325658432010,"Jacob", "Brown", "jbrown@bentley.edu", MD5("Jbrown22!"));
-INSERT INTO USER VALUES (873869103687,"Steven", "Jones", "sjones@bentley.edu", MD5("Sjones22!"));
+
+INSERT INTO USER VALUES ("188435878401", "John", "Doe", "jdoe@bentley.edu", MD5("Jdoe22!"));
+INSERT INTO USER VALUES ("325658432010","Jacob", "Brown", "jbrown@bentley.edu", MD5("Jbrown22!"));
+INSERT INTO USER VALUES ("873869103687","Steven", "Jones", "sjones@bentley.edu", MD5("Sjones22!"));
--- hx711py/weight.py
+++ hx711py/weight.py
@@ -1,75 +1,87 @@
 #! /usr/bin/python2
 import time
 import sys
+#import RPi.GPIO as GPIO #RFID
+from mfrc522 import SimpleMFRC522 #RFID
 
-sys.path.insert(0, '/home/pi/Desktop/HonorsCapstone/hd44780')
+sys.path.insert(0, '/home/camilaferno/Desktop/HonorsCapstone/hd44780')
 import lcddriver
 
-EMULATE_HX711=False
-lcd = lcddriver.lcd()
+def authenticateUser():
+    print("Welcome. Please scan your ID")
+    id, text = reader.read()
+    if id == user1["UID"]:
+        print("Welcome", user1["firstName"])
+    return True
+    
+
+user1 = {'UID': 188435878401, "firstName": "John", 'lastName': "Doe"}
+
+reader = SimpleMFRC522() #RFID
+
+EMULATE_HX711=False #weight
+lcd = lcddriver.lcd() #lcd
 
 referenceUnit = 1
 
 if not EMULATE_HX711:
-	import RPi.GPIO as GPIO
-	from hx711 import HX711
-else:
-	from emulated_hx711 import HX711
-
-def cleanAndExit():
-	print("Cleaning...")
-
-	if not EMULATE_HX711:
-		GPIO.cleanup()
-        
-	print("Bye!")
-	sys.exit()
+    import RPi.GPIO as GPIO
+    from hx711 import HX711
+#else:
+#    from emulated_hx711 import HX711
 
 try:
-    hx = HX711(5, 6)
+    #Authenticate user
+    if authenticateUser():
+        print(EMULATE_HX711)
+        hx = HX711(5, 6)
+        print("here")
+        hx.set_reading_format("MSB", "MSB")
 
-    hx.set_reading_format("MSB", "MSB")
+        # HOW TO CALCULATE THE REFFERENCE UNIT
+        # To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
+        # In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
+        # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
+        # If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
+        hx.set_reference_unit(229)
+        #hx.set_reference_unit(referenceUnit)
 
-    # HOW TO CALCULATE THE REFFERENCE UNIT
-    # To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
-    # In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
-    # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
-    # If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
-    hx.set_reference_unit(229)
-    #hx.set_reference_unit(referenceUnit)
+        hx.reset()
 
-    hx.reset()
+        hx.tare()
 
-    hx.tare()
+        print("Add weight now...")
+        time.sleep(5)
 
-    print("Add weight now...")
+        counter = 0
+        while True:
+            val = max(0, int(hx.get_weight(5)))
+            print(val)
+            if counter >= 5 and val > 0:
+                print("The weight is: ", val)
+                lcd.lcd_display_string("The weight is: ", 1)
+                lcd.lcd_display_string(str(val)+" grams", 2)
+                time.sleep(5)
+                break
+                
+            print("Calculating weight...")
+            lcd.lcd_display_string("Calculating", 1)
+            lcd.lcd_display_string("weight...", 2)
+            
+            counter += 1
 
-    prev = 0
-    while True:
-        val = max(0, int(hx.get_weight(5)))
-        print(val)
-        if val == prev and prev != 0:
-            print("The weight is: ", val)
-            lcd.lcd_display_string("The weight is: ", 1)
-            lcd.lcd_display_string(str(val)+" grams", 2)
-            time.sleep(5)
+            hx.power_down()
+            hx.power_up()
+            time.sleep(3)
             lcd.lcd_clear()
-            cleanAndExit()
-        prev = val
-        print("Calculating weight...")
-        lcd.lcd_display_string("Calculating", 1)
-        lcd.lcd_display_string("weight...", 2)
-
-        hx.power_down()
-        hx.power_up()
-        time.sleep(3)
-        lcd.lcd_clear()
+    else:
+        print("Unable to authenticate user")
 except KeyboardInterrupt:
     print("Keyboard Interrupt. Closing program...")
-except:
-    print("Error. Closing program...")
+except Exception as e:
+    print("Error:", str(e), "Closing program...")
 finally:
     lcd.lcd_clear()
     GPIO.cleanup()
-    print("Cleaning Done")
+    print("Thank you!")
     sys.exit()
